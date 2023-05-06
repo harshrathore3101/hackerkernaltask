ae91fcf8ec28eb1611fbe1c789a4329a481997bb
Harsh <harshrathore3101@gmail.com>
2023-05-06 13:05:52 +0530

    data base link

--- model/database.js
+++ model/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 exports.databaseconnection = () => {
   try {
-    mongoose.connect("mongodb://localhost/databasehk");
+    mongoose.connect("mongodb+srv://mohit123:mohit123@mohit.tjed4.mongodb.net/");
     console.log("Database connected");
   } catch (err) {
     console.log("Database Error >", err.message);
